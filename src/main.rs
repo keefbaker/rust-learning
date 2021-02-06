c2af9c12954cd4b0b9515d3086b73e9af51f3e1e
Keith Baker <keef.baker@n5md.com>
2021-02-06 21:45:19 +0000

    turn into message

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use rand::seq::SliceRandom;
 
 fn main() {
     let vs = vec!["sausage", "bacon", "eggs"];
-    println!("{:?}", vs.choose(&mut rand::thread_rng()).unwrap());
+    println!("For Breakfast I want {}", vs.choose(&mut rand::thread_rng()).unwrap());
 }
